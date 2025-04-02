430fa93676a1083e73afd5af5ccf4ad185e19b60
brook-bang <1455047806@qq.com>
2025-04-02 15:38:52 +0800

    finish middlewares

--- src/http/middlewares.rs
+++ src/http/middlewares.rs
@@ -2,3 +2,28 @@ use super::Connection;
 use super::Error;
 use super::HttpHandler;
 use async_trait::async_trait;
+
+#[derive(Clone)]
+pub struct LogMiddleware<CH>
+where
+    CH: super::HttpHandler,
+{
+    pub wrapped: CH,
+}
+
+#[async_trait]
+impl<CH> HttpHandler for LogMiddleware<CH>
+where
+    CH: HttpHandler + Sync + Send,
+{
+    async fn handle_connection(&self, connection: &mut Connection) -> Result<(), Error> {
+        println!(
+            "method:{:?}\nuri:{:?}\nversion:{:?}\nheaders:{:?}\n",
+            connection.request.method,
+            connection.request.uri,
+            connection.request.version,
+            connection.request.headers
+        );
+        self.wrapped.handle_connection(connection).await
+    }
+}
