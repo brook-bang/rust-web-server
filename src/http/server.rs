f06fd25227f5210630dcc109f8c9f021c05a5114
brook-bang <1455047806@qq.com>
2025-04-02 14:45:00 +0800

    finish server

--- src/http/server.rs
+++ src/http/server.rs
@@ -2,3 +2,41 @@ use super::Connection;
 use super::Error;
 use async_trait::async_trait;
 use tokio::net::{TcpListener, TcpStream};
+
+#[async_trait]
+pub trait HttpHandler {
+    async fn handle_connection(&self, conn: &mut Connection) -> Result<(), Error>;
+}
+
+#[derive(Clone)]
+pub struct Server;
+
+impl Server {
+    pub fn new() -> Server {
+        Server {}
+    }
+}
+
+impl Server {
+    async fn handle<CH>(self, socket: TcpStream, ch: CH) -> Result<(), Error>
+    where
+        CH: HttpHandler + Send + Sync + 'static,
+    {
+        let mut connection = Connection::new(socket).await?;
+        ch.handle_connection(&mut connection).await;
+        Ok(())
+    }
+    
+    pub async fn start<H>(addr: &str, handler: H) -> Result<(), Error>
+    where
+        H: HttpHandler + Send + Sync + Clone +'static,
+    {
+        let listener = TcpListener::bind(addr).await?;
+        let server = Server::new();
+        loop {
+            let server_clone = server.clone();
+            let (socket, _) = listener.accept().await?;
+            tokio::spawn(server_clone.handle(socket, handler.clone()));
+        }
+    }
+}
